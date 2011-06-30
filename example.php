74980f30435dae07c331b7f7266c02480f8a5705
Jared 「ジャッカル」Spencer <jklz@4pi.us>
2011-06-29 18:16:18 -0600

    Added example usage showing single and multi tracking

--- example.php
+++ example.php
@@ -3,4 +3,55 @@
 Example File
 
 **********************/
+
+require("dhl_tracking.class.php");
+
+
+
+################
+#
+# Start Class
+#
+################
+$track = new dhl_tracking('test');
+$track->setAuth("********", "********");
+
+################
+#
+# Single Request
+#
+################
+
+$track_one = 1111111111;
+$req_one = $track->single($airbill);
+
+print "\n<p>Single Result</p>\n";
+print "\n<pre>\n";
+print_r($req_one);
+print "\n</pre>";
+
+################
+#
+# Single Request
+#
+################
+
+$track_multi = array();
+$track_multi[] = 1111111111;
+$track_multi[] = 1111111112;
+$track_multi[] = 1111111113;
+$track_multi[] = 1111111114;
+$track_multi[] = 1111111115;
+$track_multi[] = 1111111116;
+$track_multi[] = 1111111117;
+$track_multi[] = 1111111118;
+$track_multi[] = 1111111119;
+
+$req_multi = $track->multipul($track_multi);
+
+print "\n<p>Multi Result</p>\n";
+print "\n<pre>\n";
+print_r($req_multi);
+print "\n</pre>";
+
 ?>
